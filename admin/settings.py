3e1246b97151cdb1010844a9a95429eea07de527
audienio <max@audien.io>
2025-01-12 14:53:22 +0530

    Fixed trusted origin

--- admin/settings.py
+++ admin/settings.py
@@ -35,10 +35,10 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://3.101.142.243/8000"
+    "http://3.101.142.243:8000"
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://3.101.142.243/8000"]
+CSRF_TRUSTED_ORIGINS = ["http://3.101.142.243:8000"]
 
 
 AUTHENTICATION_BACKENDS=[
